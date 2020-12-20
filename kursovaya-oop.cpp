f0d0b52de6329dc183ae01b17fc3bfabaa2a230f
Happiest-d <31284435+Happiest-d@users.noreply.github.com>
2020-12-21 01:38:23 +0300

    Добавлен класс Employee и исправлены дочерние классы Unemployed, Vacancy

--- kursovaya-oop.cpp
+++ kursovaya-oop.cpp
@@ -4,35 +4,32 @@
 #include "Popular_sort.h"
 using namespace std;
 
-//название фирмы, вакантная должность, заработная плата, требования к кандидату: пол, возраст, образование, общий стаж, стаж работы в последней занимаемой должности.
 
-class Unemployed {
-private:
-    string surname;
+class Employee {
+public:
     string sex;
-    int age;
     string education;
-    string profession;
-    string position;
     int last_experience;
     int sum_experience;
-    string date;
     string desired_position;
     int desired_salary;
+};
+
+class Unemployed : public Employee {
+private:
+    string surname;
+    int age;
+    string profession;
+    string last_position;
+    string date;
     
 };
 
-class Vacancy {
+class Vacancy : public Employee {
 private:
     string company;
-    string offer_position;
-    int offer_salary;
-    string offer_sex;
     int offer_min_age;
     int offer_max_age;
-    string offer_education;
-    int offer_last_experience;
-    int offer_sum_experience;
 };
 
 int main()
